8549a2fbed230d09c394acb758cd2adec79b3ead
Tom <elliott.tj@gmail.com>
2018-06-22 13:43:17 -0500

    Started assignment 2 with list

---
+++ assignment_2.py
@@ -0,0 +1,9 @@
+# 1) Create a list of names and use a for loop to output the length of each name (len() ).
+# 2) Add an if  check inside the loop to only output names longer than 5 characters.
+# 
+# 3) Add another if  check to see whether a name includes a “n”  or “N”  character.
+#       Recommends using "in" keyword to check for n, could also output name 
+# 4) Use a while  loop to empty the list of names (via pop() )
+#       condition should ensure the loop exists once the list is empty.
+
+NAMES_LIST = ['Thomas', 'Joanna', 'Malcolm', 'Declan', 'Bob']
