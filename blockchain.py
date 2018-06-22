7d9a0a5f11c4ebaba7257d9e33f4769027e32216
Tom <elliott.tj@gmail.com>
2018-06-22 09:26:53 -0500

    Added If/else statements per example

--- blockchain.py
+++ blockchain.py
@@ -17,21 +17,37 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
-    return float(input("Your transaction amount please: "))
+    user_input = float(input("Your transaction amount please: "))
+    return user_input
 
-# Get the first transaction input and add the value to the blockchain
-tx_input = get_user_input()
-add_value(tx_input)
 
-while True:
-    tx_input = get_user_input()
-    add_value(tx_input, get_last_blockchain_value())
-
-    # Output the blockchain list to the console
+def print_blockchain_elements():
+    """ Output the blockchain list to the console"""
     for block in blockchain:
         print('Outputting block')
         print(block)
+
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
+
+# Get the first transaction input and add the value to the blockchain
+tx_input = get_transaction_value()
+add_value(tx_input)
+
+while True:
+    print("please choose")
+    print("1: Add a new transaction value")
+    print("2: Output blockchain blocks")
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else: 
+        print_blockchain_elements()
+
+  
 
 print('done!')
