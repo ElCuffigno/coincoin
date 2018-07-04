68e25ed7825f34f46879ac015eacd1a4a3832c21
Tom <elliott.tj@gmail.com>
2018-07-04 17:10:41 -0500

    Use reduce with amount_received

--- blockchain.py
+++ blockchain.py
@@ -37,11 +37,13 @@ def get_balance(participant):
     # We ignore open transactions here because you shouldn't be able to spend money until you have it
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
-   
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
+    '''    
     amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+        for tx in tx_recipient:
+            if len(tx) > 0:
+                amount_received += tx[0]
+    '''    
     return amount_received - amount_sent
 
 
