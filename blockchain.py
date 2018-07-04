708870082ec9dcb48dab7a34fd49f817a447ff86
Tom <elliott.tj@gmail.com>
2018-07-04 17:11:58 -0500

    remove none reduce functions that were comments

--- blockchain.py
+++ blockchain.py
@@ -26,24 +26,11 @@ def get_balance(participant):
     tx_sender.append(open_tx_sender)
     # Calculate the total amount of coins sent
     amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
-    '''amount_sent = 0
-    # Calculate the total amount of coins sent 
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
-            '''
-
     # This fetches received coin amounts of transations that were already in the blockchain
     # We ignore open transactions here because you shouldn't be able to spend money until you have it
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
     amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
-    '''    
-    amount_received = 0
-        for tx in tx_recipient:
-            if len(tx) > 0:
-                amount_received += tx[0]
-    '''    
     return amount_received - amount_sent
 
 
