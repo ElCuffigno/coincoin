2de8c1c054ccc3c3f9355cd8fd0861e760924edc
Tom <elliott.tj@gmail.com>
2018-07-06 20:20:34 -0500

    Updated assignment with instructor examples and solution.

--- assignment_4.py
+++ assignment_4.py
@@ -1,23 +1,64 @@
 # 1) Write a normal function that accepts another function as an argument. Output the result of that other function in your “normal” function.
 
-def normal_func(funco, *args):
-    a, b = args
-    return funco(a, b)
+def normal_func(funco):
+    return(funco(5))
+
+''' EXAMPLE
+
+def transform_data(fn):
+    print(fn(10))
+
+'''
 
 # 2) Call your “normal” function by passing a lambda function – which performs any operation of your choice – as an argument.
 
-print(normal_func(lambda a, b: a + b, *[1,2]))
+print(normal_func(lambda a: a + 5))
 
+
+''' EXAMPLE
+transform_data(lambda data: data / 5)
+
+'''
 # 3) Tweak your normal function by allowing an infinite amount of arguments on which your lambda function will be executed.     
 
 def normal_func(funco, *args):
     if not args:
         return "Needs Arguments!"
-    elif len(args) == 1:
-        return args
     else:
         for arg in args:
+            print(funco(arg))
             
 
+''' EXAMPLE
+
+def transform_data2(fn, *args):
+    for arg in args:
+        print(fn(arg))
+
+transform_data2(lambda data: data / 5, 10 15, 22, 30)
+
+'''
 
 # 4) Format the output of your “normal” function such that numbers look nice and are centered in a 20 character column.
+def normal_func(funco, *args):
+    if not args:
+        return "Needs Arguments!"
+    else:
+        for arg in args:
+            answer = funco(arg)
+            print(f'{answer:^20f}')
+
+normal_func(lambda a: a + 5, 1,2,3,4)
+
+
+
+''' EXAMPLE
+
+def transform_data3(fn, *args):
+    for arg in args:
+        print('Result: {:^20.2f}'.formatfn(arg))
+
+transform_data3(lambda data: data / 5, 10 15, 22, 30)
+
+'''
+            
